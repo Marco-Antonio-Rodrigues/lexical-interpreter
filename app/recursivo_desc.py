512c93983a3f35f4e2f32d55eae2961f1d9c2515
Marco-Antonio-Rodrigues <marcoantoniorodrigues200201@gmail.com>
2024-12-05 11:29:56 -0300

    [feat]: implementation recursivo_desc

--- app/recursivo_desc.py
+++ app/recursivo_desc.py
@@ -12,5 +12,40 @@ class RecDescendente:
         self.id = -1
         self.current = None
         self.txt = ''
+        
+    def nextToken(self):
+        self.id += 1
+        if self.id < len(self.tokens):
+            self.current = self.tokens[self.id]
+        return self.current
+    
+    def currentTok(self):
+        return self.current
+        
     def start(self): 
-        return "OK:", None
+        self.nextToken()
+        a, e = self.E()
+        if self.currentTok().type != Consts.EOF:
+            return None, (e+":Error nao $ no final")
+        return a, e
+    
+    def K(self):
+        if self.currentTok().type == Consts.PLUS:
+            self.nextToken()
+            if self.currentTok().type == Consts.INT:
+                self.nextToken()
+                self.txt += "+i"
+                a, e = self.K()
+                return self.txt, e
+            else:
+                return self.txt, "erro não é inteiro (final?)"
+        self.txt += "e"
+        return self.txt, None
+    
+    def E(self):
+        if self.currentTok().type == Consts.INT:
+            self.txt += "i"
+            self.nextToken()
+            a, e = self.K()
+            return a, e
+        return None, "Falha E(), precisa iniciar com inteiro"
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -15,3 +15,12 @@ def test_deve_aceitar_quebra_de_linha():
     tokens, error = lexer.makeTokens()
     assert error is None
     assert len(tokens) == 2
+    
+def test_deve_aceitar_somas_de_numeros():
+    input_string = '1+2+3'
+    lexer = Lexer(input_string)
+    
+    tokens, error = lexer.makeTokens()
+    assert error is None
+    assert len(tokens) == 6
+    
